a37d5d9c63911326c963f4e6775b386d6d828665
Brolock <marcelin.dupraz@gmail.com>
2018-08-26 20:44:52 +0100

    Simplify image_transformation.center_to_shape() by a lot

--- generate_number_image.py
+++ generate_number_image.py
@@ -7,7 +7,6 @@ import utility
 
 from mnist_loader import get_mnist_dict, get_digits_from_dataset
 
-#Transformations HAVE to modify the given np.array(s) in place
 def generate_number_image(number, merge_digits_function,
                           final_image_transformations=[],
                           digit_wise_transformations=[]):
@@ -19,16 +18,18 @@ def generate_number_image(number, merge_digits_function,
     digit_images = get_digits_from_dataset(number, label_to_indices, data)
 
     for transformation in digit_wise_transformations:
-        transformation(digit_images)
+        digit_images = transformation(digit_images)
 
     merged_digits_image = merge_digits_function(digit_images)
 
     for transformation in final_image_transformations:
-        transformation(merged_digits_image)
+        merged_digits_image = transformation(merged_digits_image)
 
     import matplotlib.pyplot as plt
     plt.imshow(merged_digits_image, cmap="gray")
     plt.show()
+
+    return merged_digits_image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- image_transformation.py
+++ image_transformation.py
@@ -1,32 +1,24 @@
 import numpy as np
 
 def center_to_shape(image, shape, background_color=0):
+    ''' Center the image inside of an image of shape shape, crops if necessary '''
+    min_row = min(image.shape[0], shape[0]) // 2
+    min_cols = min(image.shape[1], shape[1]) // 2
 
-    center_image = np.full(shape, background_color)
 
-    extra_rows = shape[0] - image.shape[0]
-    extra_cols = shape[1] - image.shape[1]
+    # Get center of both original image and desired shape and get the indices
+    # to draw to and from by taking the minimum dimension of the two
+    center_result = (shape[0] // 2, shape[1] // 2)
+    res_box = (center_result[0] - min_row, center_result[0] + min_row,
+               center_result[1] - min_cols, center_result[1] + min_cols)
 
-    # if *_extra_space is negative, we need to crop the original image to fit
-    rows_to_crop_start = abs(min(0, extra_rows // 2))
-    rows_to_crop_end = abs(extra_rows) - rows_to_crop_start if rows_to_crop_start else 0
-    rows_to_crop_end = image.shape[1] - rows_to_crop_end
+    center_image = (image.shape[0] // 2, image.shape[1] // 2)
+    img_box = (center_image[0] - min_row, center_image[0] + min_row,
+               center_image[1] - min_cols, center_image[1] + min_cols)
 
-    cols_to_crop_start = abs(min(0, extra_cols // 2))
-    cols_to_crop_end = abs(extra_cols) - cols_to_crop_start if cols_to_crop_start else 0
-    cols_to_crop_end = image.shape[1] - cols_to_crop_end
+    res = np.full(shape, background_color)
+    res[res_box[0]:res_box[1], res_box[2]:res_box[3]] = (
+            image[img_box[0]: img_box[1], img_box[2]: img_box[3]])
 
-    # if *_extra_space is positive we need the indices where to insert
-    rows_in_center_start = max(0, extra_rows // 2)
-    rows_in_center_end = shape[0] - (extra_rows - rows_in_center_start)
+    return res
 
-    cols_in_center_start = max(0, extra_cols // 2)
-    cols_in_center_end  = shape[1] - (extra_cols - cols_in_center_start)
-
-    center_image[rows_in_center_start:rows_in_center_end,
-            cols_in_center_start:cols_in_center_end] = (
-                    image[rows_to_crop_start:rows_to_crop_end,
-                          cols_to_crop_start:cols_to_crop_end]
-                    )
-
-    image = center_image
