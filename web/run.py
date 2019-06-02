e3bdfb0ee023da95dc698017e259c030c99fe6a0
Marcelin Dupraz <marcelin.dupraz@gmail.com>
2019-06-02 08:59:30 +0200

    Use Docker for web backend And add pika for message handling
    fixup

--- web/run.py
+++ web/run.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import request
 
+import json
+import os
+
 from flask import render_template
 app = Flask(__name__)
 
@@ -13,6 +16,24 @@ def single_image():
     if request.method == "GET":
         return render_template("query_image.html")
     else:
-        userid = request.form['userid']
+        userid = request.form['user_id']
         number = request.form['number']
-        return "You enter userid={} and number={}".format(userid, number)
+        channel.basic_publish(exchange='',
+                              routing_key='web-queries',
+                              body=json.dumps({"user_id": userid,"number": number}))
+        return "You enter userid={} and number={}\n".format(userid, number)
+
+import pika
+channel = None
+
+def start_pika():
+    if "RABBITMQ_HOST" in os.environ:
+        host_dest = os.environ["RABBITMQ_HOST"]
+    else:
+        host_dest = "localhost"
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host_dest, heartbeat=0))
+    global channel
+    channel = connection.channel()
+    channel.queue_declare(queue="web-queries")
+
+start_pika()
